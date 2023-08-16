70eb709d89483ed1b4317f79b74aaa899112c5e1
Heldinnn <124900375+helderjuann@users.noreply.github.com>
2023-08-16 18:59:52 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$('.phone_ph').mask('(00) 00000-0000');
+
+function disableTextS() {
+    $('body').css('-webkit-user-select','none');
+    $('body').css('-moz-user-select','none');
+    $('body').css('-ms-user-select','none');
+    $('body').css('-o-user-select','none');
+    $('body').css('user-select','none');
+}
+
+disableTextS();
